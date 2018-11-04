cb3ea3d744ce5528a3a7221940f2c36e8a4a501f
Dmitriy <dima.demyanov.1997@gmail.com>
2018-11-04 06:16:12 +0300

    v1.1

--- src/main/java/Searcher.java
+++ src/main/java/Searcher.java
@@ -24,8 +24,49 @@ public class Searcher {
     private final static String ANNOTATIONS_TAG = ".text-container";
     private final static String ATTRIBUTE_URL = "href";
     private final static String DEFAULT_OUTPUT_FILE = "out.json";
+
+    private static Document getHTML(String target, CommandLine cmd) throws IOException {
+        Connection connection;
+        if (cmd.hasOption("proxy")) {
+            String[] proxy = cmd.getOptionValue("proxy").split(":");
+            if(proxy.length < 2)
+                throw new IllegalArgumentException("Incorrect proxy");
+            connection = Jsoup.connect(SEARCH_URL).proxy(proxy[0], Integer.parseInt(proxy[1]));
+        } else
+            connection = Jsoup.connect(SEARCH_URL);
+        return connection
+                .data(SEARCH_PARAM, target)
+                .get();
+    }
+
+    private static List<Info> getInfos(String target, CommandLine cmd) throws IOException {
+        Document document = getHTML(target, cmd);
+        List<Element> elements = document.select(PAGES_TAG);
+        List<Info> resultInfos = new ArrayList<>();
+        for (Element element : elements) {
+            List<String> greenUrls = element
+                    .select(GREENURL_TAG)
+                    .stream()
+                    .map(Element::text)
+                    .collect(Collectors.toList());
+            Element page = element.selectFirst(PAGE_TAG);
+            String title = page.text();
+            String url = page.attr(ATTRIBUTE_URL);
+            Elements annotations = element.select(ANNOTATIONS_TAG);
+            resultInfos.add(new Info(greenUrls, url, title,
+                    CollectionUtils.isEmpty(annotations)
+                            ? null
+                            : annotations.first().text()));
+        }
+        return resultInfos;
+    }
+
     public static void main(String[] args) throws IOException, ParseException {
         CommandLine cmd = new CliParser().parse(args);
+        if(cmd.hasOption("help")){
+            System.out.println("searcher.jar [-i {inputfilename}(required)] [-o {outputfilename}] [-p {proxy adress X.X.X.X:X}]");
+            return;
+        }
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(cmd.getOptionValue("input")), StandardCharsets.UTF_8
@@ -38,35 +79,7 @@ public class Searcher {
         List<List<Info>> resultDocuments = new ArrayList<>();
         while(reader.ready()) {
             String target = reader.readLine();
-            Connection connection;
-            if (cmd.hasOption("proxy")) {
-                String[] proxy = cmd.getOptionValue("proxy").split(":");
-                if(proxy.length < 2)
-                    throw new IllegalArgumentException("Incorrect proxy");
-                connection = Jsoup.connect(SEARCH_URL).proxy(proxy[0], Integer.parseInt(proxy[1]));
-            } else
-                connection = Jsoup.connect(SEARCH_URL);
-            Document document = connection
-                    .data(SEARCH_PARAM, target)
-                    .get();
-            List<Element> elements = document.select(PAGES_TAG);
-            List<Info> resultInfos = new ArrayList<>();
-            for (Element element : elements) {
-                List<String> greenUrls = element
-                        .select(GREENURL_TAG)
-                        .stream()
-                        .map(Element::text)
-                        .collect(Collectors.toList());
-                Element page = element.selectFirst(PAGE_TAG);
-                String title = page.text();
-                String url = page.attr(ATTRIBUTE_URL);
-                Elements annotations = element.select(ANNOTATIONS_TAG);
-                resultInfos.add(new Info(greenUrls, url, title,
-                        CollectionUtils.isEmpty(annotations)
-                        ? null
-                        : annotations.first().text()));
-            }
-            resultDocuments.add(resultInfos);
+            resultDocuments.add(getInfos(target, cmd));
         }
         try (BufferedWriter writer = new BufferedWriter(
                 new FileWriter( cmd.hasOption("output")?cmd.getOptionValue("output"):DEFAULT_OUTPUT_FILE))
